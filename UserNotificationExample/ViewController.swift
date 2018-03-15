597404f77c20c0574194522ed874e4f9ac3dc6f7
Zaur Giyasov <zagiyas@mera.ru>
2018-03-15 16:30:50 +0300

    example

--- UserNotificationExample/ViewController.swift
+++ UserNotificationExample/ViewController.swift
@@ -7,19 +7,53 @@
 //
 
 import UIKit
+import UserNotifications
 
 class ViewController: UIViewController {
 
+    fileprivate func notificationAutorization() {
+        let natificationOptions = UNAuthorizationOptions([.alert, .badge, .sound])
+        
+        UNUserNotificationCenter.current().requestAuthorization(options: natificationOptions) { (susses, error) in
+            
+            if let error = error {
+                print("Error: Authorization =", error.localizedDescription)
+            } else {
+                print("Authorization =", susses.description)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        notificationAutorization()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func timeNotification(inSecond: TimeInterval, completion: @escaping (_ Success: Bool) -> ()) {
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: inSecond, repeats: false)
+        
+        let content = UNMutableNotificationContent()
+        content.title = "News (content.title)"
+        content.subtitle = "Nice text (content.subtitle)"
+        content.body = "🥃 In this blok you can show ... 🚩 ⏰ Qrtyugu u iuoyh9ou piuy 987y98ujp9oigy90o (content.body)"
+        
+        let request = UNNotificationRequest(identifier: "customNotification", content: content, trigger: trigger)
+        UNUserNotificationCenter.current().add(request) { (error) in
+            if let error = error {
+                print("Error: UNNotificationRequest identifier: \"customNotification\" =", error.localizedDescription)
+                completion(false)
+            } else {
+                completion(true)
+            }
+        }
     }
-
+    
+    @IBAction func pressNotificationButton(_ sender: UIButton) {
+        timeNotification(inSecond: 2) { (sussed) in
+            print("Sussed")
+        }
+    }
 
 }
 
